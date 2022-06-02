f9f3225da5748a4c638ae6182092a2cf136f4f4c
babagee654 <75646125+babagee654@users.noreply.github.com>
2022-06-02 19:35:16 -0400

    Added dynamic panels + questions, kind of made the model for questions

--- app.js
+++ app.js
@@ -11,10 +11,107 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
+// DATA
+const data = {
+    //id
+    q1: {
+        question: 'choose a race',
+        a: { value: 'human', next: {} },
+        b: { value: 'elf', next: {} },
+        c: { value: 'orc', next: {} },
+        d: { value: 'dwarf', next: {} },
+    },
+    q2: {
+        question: 'When confronted by a giant owlbear your first instinct is to',
+        a: {
+            value: 'Fight it',
+            next: {
+                question: 'When fighting you prefer to',
+                a: { value: 'get up close and persona', next: {} },
+                b: { value: 'sneak up', next: {} },
+                c: { value: 'stay far away and shoot from a distance ', next: {} },
+                d: { value: 'cast magic', next: {} },
+            }
+        },
+        b: {
+            value: 'Run away',
+            next: {
+                question: 'Where would you run to?',
+                a: { value: 'run and never stop', next: {} },
+                b: { value: 'run and find a hiding spot to set up a trap', next: {} },
+                c: { value: 'to my party members cause they are so much stronger', next: {} },
+                d: { value: 'run into the local village and prepare them for battle', next: {} },
+            }
+        },
+        c: {
+            value: 'Try to understand the situation',
+            next: {
+                question: 'As you observe, the owlbear seems to be injured, what is your next move?',
+                a: { value: 'It’s an easy kill, go and attack.', next: {} },
+                b: { value: 'carefully show yourself to the owlbear, and try to heal it.', next: {} },
+                c: { value: 'ignore it and wait for it to pass.', next: {} },
+                d: { value: 'Now’s your chance, channel a spell.', next: {} },
+            }
+        },
+        d: {
+            value: 'Give it a treat',
+            next: {
+                question: 'The owlbear is suspicious of you and lets out a loud roar, what is your next move?',
+                a: { value: 'leave the treat on the ground and slowly back away.', next: {} },
+                b: { value: "shove the treat down the owlbear's throat.", next: {} },
+                c: { value: 'quickly cast a spell to calm the owlbear.', next: {} },
+                d: { value: "display your dominance by imitating the owlbear's roar but louder.", next: {} },
+            }
+        },
+    },
+    // q3: {
+    //     question: 'You enter a town looking for a quest, where do you go first?',
+    //     a: 'The local inn',
+    //     b: 'The town square',
+    //     c: ' The mysterious cabin in the outskirts of town',
+    //     d: 'The market, you got some things to do first',
+    //     next: {
+    //         q3a: {
+    //             question:
+    //                 'At the inn you find an innkeeper, a grumpy mercenary, and a shady hooded figure. What is your next action?',
+    //             a: 'Seduce the innkeeper',
+    //             b: 'Attempt to intimidate the mercenary',
+    //             c: 'Cast magic to detect the thoughts of the hooded figure',
+    //             d: 'Offer gold to the innkeeper for some info about the people at the inn',
+    //         },
+    //         q3b: {
+    //             question:
+    //                 ' At the town square, there is a crowd of people surrounding a caravan. What is your next move?',
+    //             a: 'Use your knowledge of the town’s history to figure out what is happening',
+    //             b: 'Lie to the caravan guard and say you’re part of it and need to get back on',
+    //             c: 'Play your instrument because you are so good at playing it',
+    //             d: 'Cast a spell to disperse the crowd and see why they are gathered',
+    //         },
+    //         q3c: {
+    //             question:
+    //                 'You arrive at the cabin but there seems to be no one answering the door. What do you do?',
+    //             a: 'Break down the door',
+    //             b: 'Climb onto a tree and try to look inside cabin',
+    //             c: 'Cast a spell to teleport somewhere inside',
+    //             d: 'Channel a spell to try and communicate with the supernatural',
+    //         },
+    //         q3d: {
+    //             question:
+    //                 'At the market there is a magician selling scrolls, a throat singer singing ballads, and a blacksmith forging weapons, who do you approach?',
+    //             a: 'The magician, you want to learn some spells',
+    //             b: 'The blacksmith, you want a new weapon',
+    //             c: 'The throat singer, you want to improve your performance skills',
+    //             d: 'Sell some of your wares to a general store cause you’re poor.',
+    //         },
+    //     }
+    // },
+};
+
+
 
 app.get('/quiz', (req, res) => {
     console.log("SHOWING QUIZ");
-    res.render("quiz.ejs");
+    res.render("quiz.ejs", { data });
 })
 
 app.get('/login', (req, res) => {
---
+++ model/question.js
@@ -0,0 +1,27 @@
+QuestionModel {
+    question: string,
+        a: {
+        value: string,
+            next: {
+            QUESTION_instance
+        }
+    },
+    b: {
+        value: string,
+            next: {
+            QUESTION_instance
+        }
+    },
+    c: {
+        value: string,
+            next: {
+            QUESTION_instance
+        }
+    },
+    d: {
+        value: string,
+            next: {
+            QUESTION_instance
+        }
+    }
+}
